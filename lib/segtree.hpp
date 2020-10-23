0cde7c9d2bd0e765017148b9489abf45460f5142
calgagi <calgagi@gmail.com>
2020-10-22 22:47:27 -0700

    segtree testing

--- lib/segtree.hpp
+++ lib/segtree.hpp
@@ -18,13 +18,13 @@ private:
             else {
                 updateHelper(2*v+1, tm+1, tr, pos, val);
             }
-            tree[v] = combine(tree[2*v], tree[2*v]+1);
+            tree[v] = combine(tree[2*v], tree[2*v+1]);
         }
         return;
     }
 
     T queryHelper(int v, int tl, int tr, int left, int right) {
-        if (tr < tl) {
+        if (left > right) {
             return identity;
         }
         if (tl == left && tr == right) {
@@ -38,17 +38,20 @@ private:
     }
 
 public:
-    segtree(int pn, function<T (T, T)> c) : n(pn), combine(c) {
-        tree.assign(4*n, identity);
+    segtree(int pn, T i, function<T (T, T)> c) : n(pn), combine(c), identity(i) {
+        tree.assign(4*pn, i);
     }
 
     void update(int pos, T val) {
+        assert(pos < n);
+        assert(pos >= 0);
         updateHelper(1, 0, n-1, pos, val);
         return;
     }
 
     T query(int left, int right) {
         assert(left >= 0);
+        assert(left <= right);
         assert(right < n);
         return queryHelper(1, 0, n-1, left, right);
     }
--- tests/segtreeTester.hpp
+++ tests/segtreeTester.hpp
@@ -6,25 +6,42 @@ private:
         function<int (int,int)> f = [](int a, int b) -> int {
             return a + b;
         };
-        segtree<int> item(10, f);
+        segtree<int> item(10, 9, f);
 
         function<float (float, float)> f2 = [](float a, float b) -> float {
             return a - b;
         };
-        segtree<float> item2(10, f2);
+        segtree<float> item2(10, 0.9, f2);
 
-        segtree<string> item3(10, [](string a, string b) -> string {
+        segtree<string> item3(10, "", [](string a, string b) -> string {
             return a + b;
         });
         return true;
     }
 
+    static bool add() {
+        segtree<int> item(100, 0, [](int a, int b) -> int {
+            return a + b;
+        });
+        for (int i = 0; i < 100; i++) {
+            item.update(i, 1);
+            if (item.query(0, i) != i+1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static bool add2() {
+        segtree<int> item(1000, 0, [&](int a, int b) -> int)
+    }
 
 public:
     static bool runTests() {
         cout << "============ segtree ============" << endl;
         map<string, function<bool()>> tests = {
-            {"instantiate", instantiate}
+            {"instantiate", instantiate},
+            {"add", add}
         };
         bool ans = true;
         for (const auto& test : tests) {
