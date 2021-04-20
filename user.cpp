3307ecc60f99977df1ffa8d859bb1b47eaf317ff
calgagi <calgagi@gmail.com>
2021-04-19 19:57:48 -0700

    update user.cpp

--- user.cpp
+++ user.cpp
@@ -1,18 +1,27 @@
-void solve() {
-    return;
-}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void preprocess();
+void solve();
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+    preprocess();
 
     int tests = 1;
-    /* cin >> tests; */
+    cin >> tests;
     for (int tc = 1; tc <= tests; tc++) {
-        /* cout << "Case #" << tc << ": "; */
+        cout << "Case #" << tc << ": ";
         solve();
     }
 
     return 0;
+}
+
+void preprocess() {
+
+}
+
+void solve() {
+
 }
