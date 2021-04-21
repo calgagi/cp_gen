5982196b981458bcae6a3e4d1af7fb04e242bd9a
calgagi <calgagi@gmail.com>
2021-04-20 19:50:25 -0700

    update user.cpp again

--- user.cpp
+++ user.cpp
@@ -1,10 +1,16 @@
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-void preprocess();
-void solve();
+
+void preprocess() {
+
+}
+
+void solve() {
+
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     preprocess();
 
@@ -16,12 +22,4 @@ int main() {
     }
 
     return 0;
-}
-
-void preprocess() {
-
-}
-
-void solve() {
-
 }
