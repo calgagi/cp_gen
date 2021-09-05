4ab282bb5bfd1e041cc50451b7de4ea6dfc85fc8
Calvin Gagliano <calgagi@gmail.com>
2021-09-04 22:00:25 -0700

    parser started

---
+++ lib/parser.hpp
@@ -0,0 +1,50 @@
+#pragma once
+class parser {
+public:
+    string input = "";
+    int idx = 0, len = 0;
+
+    parser() {
+        string line;
+        while (getline(cin, line)) input += (line += '\n');
+        len = input.length();
+    }
+
+    parser(string filename) {
+        fstream f(filename, ios_base::in);
+        string line;
+        if (f.is_open()) while (getline(f, line)) input += (line += '\n');
+        len = input.length();
+    }
+
+    bool empty() {
+        return idx >= len;
+    } 
+
+    ll get_ll() {
+        string num = "";
+        if (idx < len && input[idx] == '-') num += input[idx++];
+        while (idx < len && input[idx] >= '0' && input[idx] <= '9') num += input[idx++];
+        return stoll(num);
+    }
+
+    char get_char() {
+        return (idx < len ? input[idx++] : ' ');
+    }
+
+    void ws() {
+        while (idx < len && input[idx] == ' ' || input[idx] == '\n' || input[idx] == '\t') idx++;
+    }
+
+    string get_word() {
+        string word = "";
+        while (idx < len && ((input[idx] >= 'a' && input[idx] <= 'z') || (input[idx] >= 'A' && input[idx] <= 'Z'))) word += input[idx++];
+        return word;
+    }
+
+    string get_until_ws() {
+        string res = "";
+        while (idx < len && input[idx] != ' ' && input[idx] != '\n' && input[idx] != '\n') res += input[idx++];
+        return res;
+    }
+};
