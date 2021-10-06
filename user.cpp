c7701627189678546b36c26ef2ff1fbf35d579ca
calgagi <calgagi@gmail.com>
2021-10-05 21:21:42 -0700

    updated default user.cpp

--- user.cpp
+++ user.cpp
@@ -1,14 +1,7 @@
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+    ios_base::sync_with_stdio(0); cin.tie(0);
     mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-    cout << fixed << showpoint << setprecision(13);
 
-    int num_cases;
-    cin >> num_cases;
-    for (int case_num = 1; case_num <= num_cases; case_num++) {
-
-    }
 
     return 0;
 }
